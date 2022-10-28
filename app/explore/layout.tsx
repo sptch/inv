15c960c09d9b4c6271bbb53b84904c89961ad1cc
mholovko <mykola@spatialtech.info>
2022-10-28 21:50:36 +0200

    add Viewer to explore mode

--- app/explore/layout.tsx
+++ app/explore/layout.tsx
@@ -1,13 +1,18 @@
+import PageViewer from '@/viewer/Viewer';
 import { ExploreNav, NavWrapper } from './ExploreNav';
 import { SceneWrapper } from './SceneWrapper';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
-      <NavWrapper></NavWrapper>
-      <ExploreNav />
-      <SceneWrapper />
-      {/* {children} */}
-    </div>
+    <>
+      <PageViewer />
+      <div>
+        <NavWrapper></NavWrapper>
+        <ExploreNav />
+        <SceneWrapper />
+
+        {/* {children} */}
+      </div>
+    </>
   );
 }
